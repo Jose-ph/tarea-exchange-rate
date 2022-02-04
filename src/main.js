ddf060e8b3fb997cf9500f560f6ab64227aa8637
Jose <jose_esteban_basualdo@hotmail.com>
2022-02-04 18:10:12 -0300

    adds preventDefault
    when the consult button is clicked, it prevents the form submitting proccess

---
+++ src/main.js
@@ -0,0 +1,29 @@
+
+const $form = document.querySelector('#form');
+const $btnConsult = document.querySelector('#btn-consult')
+
+
+$btnConsult.onclick= function(e) {
+
+    const $title = document.querySelector('#title');
+    const $date = document.querySelector('#date');
+
+    
+    e.preventDefault();
+    
+}
+
+
+function getBases(){
+
+    //esta función obtendrá las bases disponibles de la api
+    //luego se utilizará esta información para completar el select
+
+}
+
+function getExchangesRateFromBase(base){
+
+    //esta función obtendrá los tipos de cambio según la base
+
+
+}
